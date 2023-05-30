5d0bc5a35deaa2956c084e4b58501e7fbede2675
guilherme david <guilhermedavidrk@gmail.com>
2023-05-30 16:42:51 -0300

    :zap: feat: animate ProgressBar component and lint

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
-      home: undefined;
-      quiz: { id: string };
-      history: undefined;
-      finish: { total: string, points: string };
+      home: undefined
+      quiz: { id: string }
+      history: undefined
+      finish: { total: string; points: string }
     }
   }
 }
