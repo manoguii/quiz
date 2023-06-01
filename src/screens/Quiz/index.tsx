eb3e587dd05ac2d0ef78bbf92934d88242fc787d
guilherme david <guilhermedavidrk@gmail.com>
2023-05-31 21:23:06 -0300

    :fire: feat: create feedback tatil

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
-import { Alert, Text, View } from 'react-native'
-
+import { Alert, BackHandler, Text, View } from 'react-native'
+import * as Haptics from 'expo-haptics'
 import { useNavigation, useRoute } from '@react-navigation/native'
 
 import { styles } from './styles'
@@ -28,6 +28,7 @@ import { ProgressBar } from '../../components/ProgressBar'
 import { THEME } from '../../styles/theme'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 import { OverlayFeedBack } from '../../components/OverlayFeedBack'
+import { Audio } from 'expo-av'
 
 interface Params {
   id: string
@@ -55,6 +56,17 @@ export function Quiz() {
 
   const route = useRoute()
   const { id } = route.params as Params
+
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require('../../assets/correct.mp3')
+      : require('../../assets/wrong.mp3')
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true })
+
+    await sound.setPositionAsync(0)
+    await sound.playAsync()
+  }
 
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
@@ -96,10 +108,12 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
-      setStatusReply(1)
       setPoints((prevState) => prevState + 1)
+      await playSound(true)
+      setStatusReply(1)
       handleNextQuestion()
     } else {
+      await playSound(false)
       setStatusReply(2)
       shakeAnimation()
     }
@@ -123,7 +137,8 @@ export function Quiz() {
     return true
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
     shake.value = withSequence(
       withTiming(3, {
         duration: 400,
@@ -227,6 +242,15 @@ export function Quiz() {
       handleNextQuestion()
     }
   }, [points])
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      handleStop,
+    )
+
+    return () => backHandler.remove()
+  }, [])
 
   if (isLoading) {
     return <Loading />
